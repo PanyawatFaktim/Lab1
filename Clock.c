c075336e345f6f755d4d25fde522e3c4abcf1dc2
PanyawatFaktim <s6704062612162@email.kmutnb.ac.th>
2025-12-09 14:15:20 +0700

    input > clock > output

---
+++ Clock.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int h,m,s;
+	scanf("%d:%d:%d",&h,&m,&s);
+	printf("hour:%d\n",h);
+	printf("minute:%d\n",m);
+	printf("second:%d\n",s);
+
+	return 0;
+}
