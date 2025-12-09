d4b4b7f955218c80d21a85b998610bb3dfc23312
PanyawatFaktim <s6704062612162@email.kmutnb.ac.th>
2025-12-09 14:23:32 +0700

    calculate VAT

--- sale.c
+++ sale.c
@@ -6,12 +6,12 @@ int main() {
 	int a,b,c,d,i,total=0;
 	scanf("%d",&a);
 	total = total+a;
-	printf("%.2f\n",total*0.95);
+	printf("%.2f\n",(total*0.95)+((total*0.95)*0.07));
 	scanf("%d",&a);
 	total = total+a;
-	printf("%.2f\n",total*0.85);
+	printf("%.2f\n",(total*0.85)+((total*0.85)*0.07));
 	scanf("%d",&a);
 	total = total+a;
-	printf("%.2f\n",total*0.70);
+	printf("%.2f\n",(total*0.70)+((total*0.70)*0.07));
 	return 0;
 }
